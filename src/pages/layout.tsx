1e784acddf442be7698100951379d9792e953783
vain0x <vainzerox@gmail.com>
2019-01-29 00:16:38 +0900

    feat: Use web font "M PLUS 1p"

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -14,6 +14,7 @@ export default (contents: JSX.Element) => (
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <title>vain0x's Page</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p:400,700&amp;subset=japanese" rel="stylesheet" />
       <link rel="stylesheet" type="text/css" media="screen" href="/index.css" />
       <link rel="icon" type="image/png" href="/favicon.jpg" />
     </head>
