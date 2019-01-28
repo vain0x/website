ca70323107bd82014a1819922105aaeb2140a1f7
vain0x <vainzerox@gmail.com>
2019-01-29 00:12:06 +0900

    refactor: Remove unused file

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,14 +2,12 @@ import * as fs from "fs"
 import { renderToString } from "hyperapp-render"
 import * as path from "path"
 import { promisify } from "util"
-import aboutPage from "../src/pages/about"
 import indexPage from "../src/pages/index"
 
 const DIST_DIR = path.resolve(__dirname, "../dist")
 
 const FILES: [string, JSX.Element][] = [
   ["", indexPage],
-  ["about", aboutPage],
 ]
 
 export const main = async () => {
