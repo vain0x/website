c25e1d6f729925dc5d5641f8bdf80a5a4328b71d
vain0x <vainzerox@gmail.com>
2019-03-02 21:34:11 +0900

    feat: 競プロ参戦記の移転に伴いリンク先とアイコンを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,6 @@ const selfIcon = (
     width={24} height={24} alt="vain0x avatar" />
 )
 
-const noteIcon = (
-  <div class="g-note-icon">n</div>
-)
-
 const qiitaIcon = (
   <div class="g-qiita-icon">Q</div>
 )
@@ -80,9 +76,9 @@ const projects: Project[] = [
 
 const articles: Project[] = [
   {
-    icon: noteIcon,
+    icon: qiitaIcon,
     title: "競プロ参戦記",
-    url: "https://note.mu/vain0x/m/m9aa8513f4e97",
+    url: "https://qiita.com/tags/%E7%AB%B6%E3%83%97%E3%83%AD%E5%8F%82%E6%88%A6%E8%A8%98",
     description: "競技プログラミングの問題を解いたあと、その考察を自分なりに書いていく連載です。",
   },
   {
